1fe3750de0a4870390a42cad903f10a393d6d41a
alekskob33 <alekskob33@yandex.ru>
2022-05-02 19:20:13 +0300

    feat: cont-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  const flatArr = matrix.flat();
+  const earsArr = flatArr.filter(item => item === '^^');
+  return earsArr.length;
 }
 
 module.exports = {
